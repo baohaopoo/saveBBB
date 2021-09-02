40a575a1f306985f0c1082c5e3bbc630bb9f3ce1
LEE MIN HEE <baohaopoo@gmail.com>
2021-09-03 01:12:02 +0900

    210903 startScene 부터 수정

---
+++ 세미최종0930/Assets/lerpcamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Utility;
+
+public class lerpcamera : MonoBehaviour
+{
+    private Transform camera;
+    public GameObject target;
+    public float CameraZ = -0.0003f;
+
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(target.transform.position.x, target.transform.position.y, CameraZ);
+        transform.position = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * 0.2f);
+    }
+}
---
+++ 세미최종0930/Assets/startcamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startcamera : MonoBehaviour
+{
+
+    public GameObject camera;
+    public GameObject target;
+    public float CameraZ;
+
+    private void FixedUpdate()
+    {
+        Vector3 TargetPos = new Vector3(target.transform.position.x, target.transform.position.y, CameraZ);
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * 2f);
+        
+    }
+
+}
